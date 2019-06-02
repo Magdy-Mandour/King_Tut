cfd194dfba8fe655fd1965c1c8604a93f9fe9160
Magdy Mandour <47599046+Magdy-Mandour@users.noreply.github.com>
2019-06-02 12:02:35 +1200

    Add files via upload

---
+++ test.js
@@ -0,0 +1,13 @@
+
+/*document.write("<p>hello from test.js js external file</p>");
+alert("hello from your mom");
+var myOldPrice = 100;
+    myNewPrice = 1000; 
+    myDiscount = myNewPrice - myOldPrice + 1600;
+document.getElementById("myPrice").innerHTML = myDiscount;
+
+var myValue = 4500;
+    marketValue = 56000;
+document.getElementById("myValue").innerHTML = marketValue - myValue;
+document.getElementById('myExperiance').innerHTML = myValue;
+document.getElementbYId("myDemand").innerHTML = myValue;*/
